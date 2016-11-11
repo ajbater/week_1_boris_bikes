0e947c54f6cb187af7cc177368a21f359e2e443d
ajbater <amanda22@hotmail.co.uk>
2016-11-11 10:35:51 +0000

    bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -2,18 +2,18 @@ require './lib/docking_station_class.rb'
 
 class Bike
 
-  attr_reader :working
+ attr_reader :working
 
-  def initialize
-    @working = true
-  end
+ def initialize
+   @working = true
+ end
 
-  def working?
-    @working
-  end
+ def working?
+   @working
+ end
 
-  def broken
-    @working = false
-  end
+ def broken
+   @working = false
+ end
 
 end
